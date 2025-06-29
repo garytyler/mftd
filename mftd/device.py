97ffbf935dd82baebb702fa1b2b33ffc779a9d46
Gary <mail@garytyler.com>
2025-06-29 18:10:45 -0500

    Devine DeviceConfig model as dataclass

--- mftd/device.py
+++ mftd/device.py
@@ -1,10 +1,63 @@
 from collections.abc import MutableMapping
+from dataclasses import field, dataclass
 
 from .constants import (
     MidiChannel,
     SysexBool,
     SideSwitchAction,
 )
+
+
+@dataclass
+class DeviceConfig:
+    system_midi_channel: MidiChannel = field(
+        default=MidiChannel.SYSTEM,
+        metadata={"addr": 0},
+    )
+    bank_side_buttons: SysexBool = field(
+        default=SysexBool.TRUE,
+        metadata={"addr": 1},
+    )
+    left_button_1_function: SideSwitchAction = field(
+        default=SideSwitchAction.CC_HOLD,
+        metadata={"addr": 2},
+    )
+    left_button_2_function: SideSwitchAction = field(
+        default=SideSwitchAction.PREV_BANK,
+        metadata={"addr": 3},
+    )
+    left_button_3_function: SideSwitchAction = field(
+        default=SideSwitchAction.CC_HOLD,
+        metadata={"addr": 4},
+    )
+    right_button_1_function: SideSwitchAction = field(
+        default=SideSwitchAction.CC_HOLD,
+        metadata={"addr": 5},
+    )
+    right_button_2_function: SideSwitchAction = field(
+        default=SideSwitchAction.NEXT_BANK,
+        metadata={"addr": 6},
+    )
+    right_button_3_function: SideSwitchAction = field(
+        default=SideSwitchAction.CC_HOLD,
+        metadata={"addr": 7},
+    )
+    super_knob_start: int = field(
+        default=63,
+        metadata={"addr": 8},
+    )
+    super_knob_end: int = field(
+        default=127,
+        metadata={"addr": 9},
+    )
+    rgb_led_brightness: int = field(
+        default=127,
+        metadata={"addr": 31},
+    )
+    indicator_global_brightness: int = field(
+        default=127,
+        metadata={"addr": 32},
+    )
 
 
 class DeviceConfig(MutableMapping):
