07b47c43b99e1ebd2ec698373b0dcc6518ffa384
Gary <mail@garytyler.com>
2025-06-30 23:00:28 -0500

    Setup/teardown device configs in e2e tests

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from mftd.device import DeviceConfig
+from mftd.encoder import EncoderConfig
 from mftd.mft import MftSysexApi, MidiFighterTwister
 from mftd.midi import (
     create_midi_input,
@@ -33,16 +34,38 @@ def midi_out():
 
 
 @pytest.fixture(scope="session")
+def api():
+    yield MftSysexApi
+
+
+@pytest.fixture(scope="session")
 def mft(midi_out, midi_in):
     yield MidiFighterTwister(midi_out=midi_out, midi_in=midi_in)
 
 
 @pytest.fixture(scope="session", autouse=True)
-def device_config(midi_out, midi_in, mft):
-    device_config_in_data = MftSysexApi.get_device_config(midi_out, midi_in)
+def device_config(midi_out, midi_in, mft, api):
+    device_config_in_data = api.get_device_config(midi_out, midi_in)
     if not device_config_in_data:
         RuntimeError("Failed to get device config")
     device_config = DeviceConfig.from_in_dict(device_config_in_data)
+
     yield device_config
+
     out_data = device_config.to_out_dict()
-    MftSysexApi.set_device_config(midi_out, out_data)
+    api.set_device_config(midi_out, out_data)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def _encoder_configs(device_config, midi_out, midi_in, mft, api):
+    encoder_configs = []
+    for n in range(0, 16):
+        in_data = api.get_encoder_config(midi_out, midi_in, n)
+        config = EncoderConfig.from_in_dict(in_data)
+        encoder_configs.append((n, config))
+
+    yield
+
+    for n, config in encoder_configs:
+        out_data = config.to_out_dict()
+        api.set_encoder_config(midi_out, n, out_data)
