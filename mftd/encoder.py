14c7bd46167051bc3f1bd4e9963936ee3e2474b3
Gary <mail@garytyler.com>
2025-07-06 17:07:38 -0500

    Fix bad revert

--- mftd/encoder.py
+++ mftd/encoder.py
@@ -86,8 +86,6 @@ class EncoderConfig(BaseModel):
             return value + 1
         elif name == "switch_midi_type":  # Appears no longer used
             return value - 1
-        elif name == "encoder_shift_midi_channel":
-            return value - 1
         else:
             return value
 
@@ -99,26 +97,5 @@ class EncoderConfig(BaseModel):
             return value - 1
         elif name == "switch_midi_type":  # Appears no longer used
             return value + 1
-        elif name == "encoder_shift_midi_channel":
-            return value + 1
         else:
             return value
-
-
-EncoderConfig(
-    detent=SysexBool.TRUE,
-    movement_type=EncoderMovementType.DIRECT_HIGH_RESOLUTION,
-    switch_action_type=EncoderSwitchActionType.CC_HOLD,
-    switch_midi_channel=MidiChannel.SWITCH_AND_COLOR,
-    switch_midi_number=0,
-    switch_midi_type=0,
-    encoder_midi_channel=MidiChannel.ROTARY_ENCODER,
-    encoder_midi_number=0,
-    encoder_midi_type=EncoderMidiMessageType.SEND_CC,
-    active_color=ColorValue.DEFAULT_ACTIVE,
-    inactive_color=ColorValue.DEFAULT_INACTIVE,
-    detent_color=DetentColorValue.RED,
-    indicator_display_type=EncoderIndicatorDisplayType.BLENDED_BAR,
-    is_super_knob=SysexBool.FALSE,
-    encoder_shift_midi_channel=MidiChannel.SHIFT,
-)
